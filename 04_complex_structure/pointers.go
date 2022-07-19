6c5afcb6b3c462cc26335d89cd52fdf1dba2c800
marymarty104@gmail.com <marymarty104@gmail.com>
2022-07-19 08:36:48 +0200

    thinking about pointers

---
+++ 04_complex_structure/pointers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+    var name string
+    var namePointer *string
+
+    fmt.Println(name)
+    fmt.Println(&name)
+    fmt.Println(namePointer)
+
+}
+
+
