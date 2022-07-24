0197a5932db200a23cd8305029244d86b1a52897
marymarty104@gmail.com <marymarty104@gmail.com>
2022-07-24 17:27:18 +0200

    Adding Array basic syntax

---
+++ 02_basic_syntax/array_syntax.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main() {
+  // array with the length of 3 containing som int's
+  var arr1 = [3]int{1,2,3}
+  arr2 := [5]int{4,5,6,7,8}
+
+  fmt.Println(arr1)
+  fmt.Println(arr2)
+}
