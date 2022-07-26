76ff74f98f05c99db1700d661a48b68769b59e2e
marymarty104@gmail.com <marymarty104@gmail.com>
2022-07-26 09:55:46 +0200

    Playing with data structure

---
+++ 03_datas_structure/data_structure.go
@@ -0,0 +1,15 @@
+package main
+import (
+ "fmt"
+)
+
+
+func main() {
+ var names [4]string 
+ names := {"thomas", "nicolas", "albert", "franck"}
+ fmt.Println(names)
+
+ // var x [20] string
+
+
+}
