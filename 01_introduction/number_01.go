8e8710eb1b0c498f934aaf8d4f501bf79f8dad53
marymarty104@gmail.com <marymarty104@gmail.com>
2022-08-01 09:03:47 +0200

    numbers

--- 01_introduction/number_01.go
+++ 01_introduction/number_01.go
@@ -7,11 +7,17 @@ import (
 func main() {
 	fmt.Println(mean([]int{1, 2, 3}))    // 2
 	fmt.Println(mean([]int{1, 2, 3, 4})) // 2.5
+	fmt.Println(sum([]int{1, 2, 3}))     // 6
 }
 
 func mean(nums []int) float64 {
 	s := sum(nums)
 	return float64(s) / float64(len(nums))
+}
+
+func max(nums []int) float64 {
+	mx := max(nums)
+	return float64(mx)
 }
 
 func sum(nums []int) int {
