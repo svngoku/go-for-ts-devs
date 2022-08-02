c9bfd60ad2555900ab74c2b05695eeb5179636b6
marymarty104@gmail.com <marymarty104@gmail.com>
2022-08-02 08:09:59 +0200

    Ãplaying with arrays

--- 02_basic_syntax/array_syntax.go
+++ 02_basic_syntax/array_syntax.go
@@ -1,12 +1,14 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 func main() {
-  // array with the length of 3 containing som int's
-  var arr1 = [3]int{1,2,3}
-  arr2 := [5]int{4,5,6,7,8}
+	// array with the length of 3 containing som int's
+	var arr1 = [3]int{1, 2, 3}
+	arr2 := [5]int{4, 5, 6, 7, 8}
 
-  fmt.Println(arr1)
-  fmt.Println(arr2)
+	fmt.Println(arr1)
+	fmt.Println(arr2)
 }
