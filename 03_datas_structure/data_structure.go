31e62e80f1c1dd3c3de02b7e305c31ac71c908d1
marymarty104@gmail.com <marymarty104@gmail.com>
2022-08-31 09:12:46 +0200

    Playing with strings

--- 03_datas_structure/data_structure.go
+++ 03_datas_structure/data_structure.go
@@ -1,15 +1,30 @@
 package main
+
 import (
- "fmt"
+	"fmt"
 )
 
+// Write a function that takes a string as input and returns the string reversed.
+func reversed(f string) string {
+	n := 0
+	rune := make([]rune, len(f))
+	for _, r := range f {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+	// Convert back to UTF-8.
+	output := string(rune)
+	fmt.Println(output)
+
+	return output
+}
 
 func main() {
- var names [4]string 
- names := {"thomas", "nicolas", "albert", "franck"}
- fmt.Println(names)
-
- // var x [20] string
-
-
+	var name string = "The quick brown 狐 jumped over the lazy 犬"
+	reversed(name)
 }
